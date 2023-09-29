c054a784b64dffac5104aed1087795483f8fd3b9
Mujeeb Rahman <46838020+mujeeb129@users.noreply.github.com>
2023-09-29 10:42:48 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./styles.css";
+import ExpenseItem from "./components/ExpenseItem"
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Lets begin</h1>
+      <ExpenseItem />
     </div>
   );
 }
